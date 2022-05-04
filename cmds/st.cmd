421bdb192af21b932caa315bfdf54b11454685ca
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2022-05-04 14:22:50 +0200

    The first working e3 version.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -1,3 +1,10 @@
 # This should be a test or example startup script
 
 require s7nodave
+
+s7nodaveConfigureIsoTcpPort("cowPLC", "192.168.1.70",0,0,0)
+
+#dbLoadRecords("$(s7nodave_DB)/example.db", "P=test")
+dbLoadRecords("$(s7nodave_DB)/lo-update.template", "P=test:, R='',S=ctest, OUT='cowPLC DB3010.DBW132 uint16'")
+dbLoadRecords("$(s7nodave_DB)/ao-update.template", "P=test:, R='',S=catest, OUT='cowPLC DB3010.DBD170 float'")
+
